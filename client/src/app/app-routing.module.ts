c5a2eb88ddb6a8a569a88dd46ef84fbe943b6897
christopher-matthews1 <christopher.matthews@jbhunt.com>
2021-09-11 19:49:45 -0400

    Add Location template/sytles/routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { AllTeamsComponent } from './all-teams/all-teams.component';
 import { TeamComponent } from './team/team.component';
+import { LocationComponent } from './location/location.component';
 
 const fallbackRoute: Route = {
   path: '**', component: HomeComponent
@@ -15,6 +16,7 @@ const routes: Routes = [{
   children: [
       { path: '', component: HomeComponent },
       { path: 'about', component: AboutComponent },
+      { path: 'location', component: LocationComponent },
       { path: 'teams', component: AllTeamsComponent },
       { path: 'teams/team', component: TeamComponent },
       fallbackRoute
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AllTeamsComponent } from './all-teams/all-teams.component';
 import { TeamDetailsShortComponent } from './team-details-short/team-details-short.component';
 import { TeamComponent } from './team/team.component';
 import { PlayerComponent } from './player/player.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { PlayerComponent } from './player/player.component';
     AllTeamsComponent,
     TeamDetailsShortComponent,
     TeamComponent,
-    PlayerComponent
+    PlayerComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
